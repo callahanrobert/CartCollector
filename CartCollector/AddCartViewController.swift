32f948e69d4be79705d70a9978eeea0afcda5fbb
Robert Callahan <robert.callahan@mail.com>
2018-05-23 22:57:37 -0700

    Generating TableView from Firebase DB.  Can manually add cart info.

---
+++ CartCollector/AddCartViewController.swift
@@ -0,0 +1,88 @@
+//
+//  AddCartViewController.swift
+//  CartCollector
+//
+//  Created by Jackie Callahan on 5/21/18.
+//  Copyright © 2018 Robert Callahan. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class AddCartViewController: UIViewController {
+
+    @IBOutlet weak var CartStoreName: UITextField!
+    @IBOutlet weak var CartAddLatitude: UITextField!
+    @IBOutlet weak var CartAddLongitude: UITextField!
+    
+    var ref: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Add Firebase Database Reference        
+        ref = Database.database().reference()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func CartAddManually(_ sender: Any) {
+        //Get current timestamp
+        let todaysDate:Date = Date()
+        let dateFormatter:DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss ZZZ"
+        let DateInFormat:String = dateFormatter.string(from: todaysDate as Date)
+        
+        /* //Add info to database
+        ref?.child("Carts").childByAutoId().setValue([
+            "Located": [
+                "Latitude": CartAddLatitude.text!,
+                "Longitude": CartAddLongitude.text!
+            ],
+            "PictureRef": [
+                "UploadedImageURL": "http://googlestore.com/SomeFile.jpg"
+            ],
+            "CartOwner":[
+                "StoreName": CartStoreName.text!
+            ],
+            "DateFound":[
+                "Date": DateInFormat
+            ]
+        ])
+        */
+
+        //Add info to database
+         ref?.child("Carts").childByAutoId().setValue([
+         "Latitude": CartAddLatitude.text!,
+         "Longitude": CartAddLongitude.text!,
+         "UploadedImageURL": "http://googlestore.com/SomeFile.jpg",
+         "StoreName": CartStoreName.text!,
+         "Date": DateInFormat
+         ])
+        
+        //Return to calling screen
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func CartCancelManually(_ sender: Any) {
+        //Return to calling screen
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CartCollector/ViewController.swift
+++ CartCollector/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ref: DatabaseReference?
+    var databaseHandle: DatabaseHandle?
+    
+    // postdata string in example
+    var rData = [String]()
+    var sData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Set the Firebase reference
+        ref = Database.database().reference()
+        
+        // Retrieve the carts and listen for changes
+        databaseHandle = ref?.child("Carts").observe(.childAdded, with: { (snapshot) in
+        
+            //let uniqueKey = snapshot.key
+            //print(uniqueKey)
+            
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            
+            let varStoreName = dictionary["StoreName"] as? String
+            //let varDate = dictionary["Date"] as? String
+            let varLat = dictionary["Latitude"] as? String
+            let varLong = dictionary["Longitude"] as? String
+            //let varURL = dictionary["UploadedImageURL"] as? String
+            
+            if let storeNameExist = varStoreName {
+                // Append the data to the rData array
+                self.rData.append(storeNameExist)
+                
+                //if let LatExist = varLat, let LongExist = varLong {
+                let varLoc = varLat! + ", " + varLong!
+                    self.sData.append(varLoc)
+                //}
+                
+                // Reload the tableview
+                self.tableView.reloadData()
+            }
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +65,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rData.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "mycell")
+        //cell.textLabel?.text="row#\(indexPath.row)"
+        //cell.detailTextLabel?.text="subtitle#\(indexPath.row)"
+ 
+    
+        cell.textLabel?.text = rData[indexPath.row] as String
+        cell.detailTextLabel?.text = sData[indexPath.row] as String
+        
+        
+        return cell
+    }
+    
 }
 
